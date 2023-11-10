b6b207410b033ddd4131bb96903deecb9b4b0b48
m <m@mcarr.one>
2023-11-09 20:50:42 -0800

    yipppee

--- server/app.py
+++ server/app.py
@@ -4,6 +4,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+users = {
+    "lavilia": "Manuel"
+}
+
+@app.route("/api/user/<username>")
+def get_user(username):
+    """ get the user from the database """
+    username = username.lower()
+    if username not in users:
+        return "Error user not found"
+
+    return users[username]
+
+
 @app.route("/")
 def serve_index():
     return """
