5a04672d056dee242be043b6d3f1f34629129c48
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2023-11-09 21:24:20 -0800

    first class (users)

--- server/app.py
+++ server/app.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from dataclasses import dataclass
 
 app = Flask(__name__)
 
 users = {
     "lavilia": "Manuel"
 }
+
+@dataclass
+class UsersSlay(object):
+    username: str
+    password: str
+    name: str
+    age: int
+    associations: str
+    job: str = "Student at UCSC"
+
 
 @app.route("/api/user/<username>")
 def get_user(username):
