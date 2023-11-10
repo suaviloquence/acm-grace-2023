3b7e7edeb43ff87ddfb9d710d416cb420d3da795
m <m@mcarr.one>
2023-11-09 22:06:08 -0800

    user create rout

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
 
-from flask import Flask
+from flask import Flask, request
 from dataclasses import dataclass
 from db import DB
 from user import UsersSlay
@@ -11,6 +11,9 @@ app = Flask(__name__)
 
 db = DB()
 
+def error(message):
+    return json.dumps({"error": message})
+
 @app.route("/api/user/<username>")
 def get_user(username):
     """ get the user from the database """
@@ -18,7 +21,22 @@ def get_user(username):
     if user != None:
         return(json.dumps(user))
     else:
-        return "ERROR!!!!!!!!!!!!!!1"
+        return error("user not found")
+    
+@app.route("/api/user/<username>", methods=["POST"])
+def create_user(username):
+    user = UsersSlay.get(db, username)
+    # make sure is none, otherwise return error
+
+    if not request.is_json():
+        # return ererr invalid request
+        return ...
+    
+    data = request.json
+    data['username']
+    user = UsersSlay(...) # TODO
+    
+    user.create()
 
 
 @app.route("/")
