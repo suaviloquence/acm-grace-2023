5efceeb9bc09ad07971b3830ce3cd4f6b580868c
m <m@mcarr.one>
2023-11-09 22:12:49 -0800

    user crud

--- server/user.py
+++ server/user.py
@@ -34,6 +34,7 @@ class UsersSlay(object):
 		cur = db.con.cursor()
 		cur.execute("INSERT INTO users (username, password, name, pfp, age, year) VALUES (?, ?, ?, ?, ?)", self.username, self.password, self.pfp_id, self.age, self.year)
 	
+	@staticmethod
 	def create_pfp(db: DB, pfp: bytes):
 		cur = db.con.cursor()
 		cur.execute("INSERT INTO pfps (data) VALUES (?) RETURNING id", pfp)
@@ -41,4 +42,23 @@ class UsersSlay(object):
 		if res is None:
 			return 0
 		return res['id']
+	
+	def update(self, db: DB):
+		cur = db.con.cursor()
+		cur.execute("UPDATE users SET password = ?, name = ?, pfp = ?, age = ?, year = ? WHERE username = ?",
+					self.password, self.name, self.pfp, self.age, self.year)
+		
+	@staticmethod
+	def update_pfp(db: DB, pfp_id: int, pfp: bytes):
+		cur = db.con.cursor()
+		cur.execute("UPDATE pfps SET data = ? WHERE id = ?", pfp, pfp_id)
+	
+	def delete(self, db: DB):
+		cur = db.con.cursor()
+		cur.execute("DELETE FROM users WHERE username = ?", self.username)
+	
+	@staticmethod
+	def delete(db: DB, pfp_id: int):
+		cur = db.con.cursor()
+		cur.execute("DELETE FROM pfps WHERE id = ?", pfp_id)
 
