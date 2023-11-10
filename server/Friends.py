b1ed1f08457b4626fbdaf29cb9cdfad6495e121d
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2023-11-09 22:47:47 -0800

    rahhhhh

---
+++ server/Friends.py
@@ -0,0 +1,17 @@
+from server.user import UsersSlay
+from dataclasses import dataclass
+"""
+initialize friend list for each user
+
+user requests someone else
+
+user receives request
+- choose whether to accept or not
+
+saves user friend list
+
+removes friend?
+
+"""
+
+
